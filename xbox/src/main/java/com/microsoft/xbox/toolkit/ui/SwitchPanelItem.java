909684143ea1fd8c239e09cb50d65cb00eaf145e
Иван Тимашков <timscriptov@mail.ru>
2023-04-20 01:48:34 +0500

    Update SwitchPanelItem.java

--- xbox/src/main/java/com/microsoft/xbox/toolkit/ui/SwitchPanelItem.java
+++ xbox/src/main/java/com/microsoft/xbox/toolkit/ui/SwitchPanelItem.java
@@ -3,6 +3,7 @@ package com.microsoft.xbox.toolkit.ui;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
@@ -22,10 +23,10 @@ public class SwitchPanelItem extends FrameLayout implements SwitchPanel.SwitchPa
     public SwitchPanelItem(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         TypedArray obtainStyledAttributes = context.obtainStyledAttributes(attributeSet, XLERValueHelper.getStyleableRValueArray("SwitchPanelItem"));
-        this.state = obtainStyledAttributes.getInteger(XLERValueHelper.getStyleableRValue("SwitchPanelItem_state"), -1);
+        this.state = obtainStyledAttributes.getInteger(XLERValueHelper.getStyleableRValue("SwitchPanelItem_state"), INVALID_STATE_ID);
         obtainStyledAttributes.recycle();
         if (this.state >= 0) {
-            setLayoutParams(new RelativeLayout.LayoutParams(-1, -1));
+            setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
             return;
         }
         throw new IllegalArgumentException("You must specify the state attribute in the xml, and the value must be positive.");
