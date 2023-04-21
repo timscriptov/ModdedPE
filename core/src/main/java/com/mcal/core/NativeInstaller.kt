cd86136d84b3b365350ad1ddd888355decb1084b
Иван Тимашков <timscriptov@mail.ru>
2023-04-21 20:08:22 +0500

    Update NativeInstaller.kt

--- core/src/main/java/com/mcal/core/NativeInstaller.kt
+++ core/src/main/java/com/mcal/core/NativeInstaller.kt
@@ -9,19 +9,19 @@ import com.mcal.core.utils.FileHelper
 import com.mcal.core.utils.Patcher
 import java.io.File
 import java.lang.reflect.InvocationTargetException
+import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
 
 class NativeInstaller(private val context: Context) {
-
     fun install() {
         val nativeDir = nativeDir(context)
         val abi = getNameAppABI(context)
-        if (abi.contains("arm64-v8a")) {
-            extractNative(nativeDir)
-        } else if (abi.contains("armeabi-v7a")) {
-            extractNative(nativeDir)
-        } else if (abi.contains("x86")) {
-            extractNative(nativeDir)
+        if (abi.contains("arm64-v8a") || (abi.contains("armeabi-v7a")) ||
+            (abi.contains("x86_64")) || (abi.contains("x86"))
+        ) {
+            extractLibraries(nativeDir)
+            patchingMinecraftLibrary()
+            loadLibraries()
         }
         try {
             Patcher.patchNativeLibraryDir(
@@ -44,25 +44,63 @@ class NativeInstaller(private val context: Context) {
     }
 
     @SuppressLint("UnsafeDynamicallyLoadedCode")
-    private fun extractNative(libDir: File) {
-        val nativeLibrariesFile = getNativeLibrariesFile(context)
+    private fun extractLibraries(nativeDir: File) {
+        val tmpLibLokiCraftFile = getNativeLibrariesFile(context)
         arrayListOf(
             "libc++_shared.so",
             "libfmod.so",
-            "libminecraftpe.so",
+            "liblokicraft.so",
             "libMediaDecoders_Android.so"
         ).forEach { libName ->
-            if (nativeLibrariesFile.exists()) {
-                val nativeLibrariesZipFile = ZipFile(nativeLibrariesFile)
-                nativeLibrariesZipFile.getInputStream(nativeLibrariesZipFile.getEntry(libName))
-                    .use { input ->
-                        val libFilePath = File(libDir, libName)
-                        FileHelper.writeToFile(libFilePath, FileHelper.readAllBytes(input))
-                        if (libFilePath.exists()) {
-                            System.load(libFilePath.path)
-                        }
-                    }
+            val libFilePath = if (libName.contains("liblokicraft.so")) {
+                File(nativeDir, "libminecraftpe.so")
+            } else {
+                File(nativeDir, libName)
+            }
+            ZipFile(tmpLibLokiCraftFile).getInputStream(ZipEntry(libName))
+                ?.let {
+                    FileHelper.writeToFile(libFilePath, it)
+                }
+        }
+    }
+
+    private fun patchingMinecraftLibrary() {
+        val libraryFile = getNativeLibrariesFile(context)
+
+        val libraryBytes = libraryFile.readBytes()
+        val origImgBytes = context.assets.open("resources/title_original.png").readBytes()
+        val newImgBytes = context.assets.open("resources/title_replace.png").readBytes()
+
+        val indexOfOrigImg = hexIndexOf(libraryBytes, origImgBytes)
+        if (indexOfOrigImg == -1) {
+            println("title.png not found in libminecraftpe.so")
+            return
+        }
+
+        val libraryPatchedBytes = libraryBytes.copyOf()
+        System.arraycopy(newImgBytes, 0, libraryPatchedBytes, indexOfOrigImg, newImgBytes.size)
+
+        FileHelper.writeToFile(libraryFile, libraryPatchedBytes)
+    }
+
+    @SuppressLint("UnsafeDynamicallyLoadedCode")
+    private fun loadLibraries() {
+        nativeDir(context).walk().maxDepth(1).filter { it.isFile }.forEach {
+            if (it.name.endsWith(".so")) {
+                System.load(it.path)
             }
         }
+    }
+
+    private fun hexIndexOf(source: ByteArray, target: ByteArray): Int {
+        outer@ for (i in 0..source.size - target.size) {
+            for (j in target.indices) {
+                if (source[i + j] != target[j]) {
+                    continue@outer
+                }
+            }
+            return i
+        }
+        return -1
     }
 }
