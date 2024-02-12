12ca27e39f4d50b58ec150c18d8cd1ad8421d6b9
Иван Тимашков <timscriptov@mail.ru>
2024-02-12 11:07:59 +0300

    Update GameActivity.kt

--- minecraft-app/src/main/java/com/mcal/moddedpe/GameActivity.kt
+++ minecraft-app/src/main/java/com/mcal/moddedpe/GameActivity.kt
@@ -9,7 +9,6 @@ import android.os.Bundle
 import android.text.Html
 import android.text.Spanned
 import android.view.KeyEvent
-import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mcal.moddedpe.task.CustomServers
 import com.mcal.moddedpe.task.MapsInstaller
@@ -60,15 +59,26 @@ class GameActivity : MainActivity() {
 
     private fun showAgreeDialog() {
         if (isAgreePrivacyPolice()) {
+            val gamePrivacyPolice = "https://github.com/timscriptov/ModdedPE/tree/master/PrivacyPolicy/README.md"
+            val xboxPrivacyPolice = "https://privacy.microsoft.com/en-us/privacystatement"
+            val minecraftPrivacyPolice = "https://account.mojang.com/terms#privacy"
             MaterialAlertDialogBuilder(this).apply {
-                setTitle("Title")
-                setMessage(fromHtml("<body><p>Content</p></body>"))
+                setTitle("ModdedPE Privacy Policy")
+                setMessage(
+                    fromHtml(
+                        "<p>Xbox Privacy Policy</p>" +
+                                "<p>ModdedPE collects account information to allow autocompleting Xbox Login details,and the information is only used to login Xbox.</p>" +
+                                "<p>The Xbox Login functionality's privacy policy can be found at <a href=\"$xboxPrivacyPolice\">$xboxPrivacyPolice</a></p>" +
+                                "<br>" +
+                                "<p>MinecraftPE Privacy Policy</p>" +
+                                "<p>Some anonymous information is collected and sent to Mojang to improve MinecraftPE.</p>" +
+                                "<p>Mojang's Privacy Policy can be found at <a href=\"$minecraftPrivacyPolice\">$minecraftPrivacyPolice</a></p></body>"
+                    )
+                )
                 setCancelable(false)
-                setNeutralButton("Privacy Policy") { _: DialogInterface, _: Int ->
-                    startActivity(Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse("http://localhost:1234")
-                    ))
+                setNeutralButton("Privacy Policy") { dialogInterface: DialogInterface, _: Int ->
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(gamePrivacyPolice)))
+                    show()
                 }
                 setPositiveButton("Agree") { dialogInterface: DialogInterface, _: Int ->
                     setAgreePrivacyPolice(false)
