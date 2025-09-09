d425ea7ca819535d2e61dd034808090a22fd7320
Иван Тимашков <timscriptov@mail.ru>
2025-09-09 17:11:42 +0500

    Update gradle and dependencies

--- httpclient/build.gradle.kts
+++ httpclient/build.gradle.kts
@@ -20,6 +20,24 @@ android {
             )
         }
     }
+    packaging {
+        resources {
+            excludes += listOf(
+                "META-INF/AL2.0",
+                "META-INF/LGPL2.1",
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/license.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/notice.txt",
+                "META-INF/ASL2.0",
+                "META-INF/INDEX.LIST",
+                "META-INF/*.kotlin_module"
+            )
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
